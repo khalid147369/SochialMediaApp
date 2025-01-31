fdc200390cd195fd3e40f45676a1fc204ca9c784
khalid147369 <113128365+khalid147369@users.noreply.github.com>
2025-01-31 14:41:31 +0100

    Delete SocialMediaApp directory

--- SocialMediaApp/DTO/SavedPostsDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SocialMediaApp.DTO
-{
-    public class SavedPostsDTO
-    {
-        public string UserId { get; set; } // User's ID (FK from AspNetUsers)
-        public int PostId { get; set; }
-    }
-}
