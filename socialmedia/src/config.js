50213b857549f027a0b60967438247da8422a608
khalid <rabaaouikhalid@gmail.com>
2025-02-15 10:59:45 +0100

    addContact

--- socialmedia/src/config.js
+++ socialmedia/src/config.js
@@ -1,6 +1,6 @@
 
-export const backendUrl = 'http://localhost:5184'
-export const backendUrlWs = 'ws://localhost:5184'
-// export const backendUrl = 'https://socialmediaapp.runasp.net';
-// export const backendUrlWs = 'wss://socialmediaapp.runasp.net';
+// export const backendUrl = 'http://localhost:5184'
+// export const backendUrlWs = 'ws://localhost:5184'
+export const backendUrl = 'https://socialmediaapp.runasp.net';
+export const backendUrlWs = 'wss://socialmediaapp.runasp.net';
 
